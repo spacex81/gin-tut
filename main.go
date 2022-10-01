f80d89434810f98e191d11ff1f199b65d3dbb030
= <=>
2022-10-01 14:24:03 +0900

    boilerplate

--- main.go
+++ main.go
@@ -4,11 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
 	router.Run()
 }
-
